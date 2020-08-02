382904ab562757b070dc3b87b52f64cd7903801a
Ping Chen <remotenonsense@gmail.com>
2020-08-02 10:40:49 +0900

    Bump to tauri.js 0.11 and tauri crate 0.8

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ spago bundle-module -m Main -t output/Main/index.js
 mkdir -p dist
 cp index.html dist
 NODE_ENV=production yarn build:css
-yarn esbuild --bundle --minify --outdir=dist main.js
+esbuild --bundle --minify --outdir=dist main.js
 tauri build
